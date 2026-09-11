10888afb09f0a2fb98334bfe5624c2ad1520bfe5
suthisakm <suthisak.m@intersendmoney.com>
2026-09-11 18:14:28 +0700

    Disable dashboard cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { supabaseAdmin } from "@/lib/supabase-server";
-
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export default async function Home() {
   const { data: latestPrice } = await supabaseAdmin
     .from("market_prices")
